6e8c5789de910d5e0c6c879e523555d68c2faed1
Yasmin Ventura <yasmin.gomsv@gmail.com>
2024-04-12 11:46:40 -0300

    v1.0.2 - Add uma sobrecarga no construtor Product

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,30 +10,27 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        Product p = new Product();
 
         System.out.println("Enter product data: ");
         System.out.print("Name: ");
-        String name = sc.nextLine();  //as variaveis passam a ser temporarias
+        String name = sc.nextLine();
         System.out.print("Price: ");
         double price = sc.nextDouble();
-        System.out.print("Quantity in stock: ");
-        int quantity = sc.nextInt();
 
-        Product product = new Product(name, price, quantity);
+        Product product = new Product(name, price);
 
         System.out.println();
         System.out.println("Product data: " + product);
 
-        //atualizar o estoque
         System.out.println();
         System.out.print("Enter the number of products to be added in stock: ");
-        quantity = sc.nextInt();
+        int quantity = sc.nextInt();
         product.addProducts(quantity);
 
         System.out.println();
         System.out.println("Updated data: " + product);
 
-        //saida estoque
         System.out.println();
         System.out.print("Enter the number of products to be removed in stock: ");
         quantity = sc.nextInt();
--- src/entities/Product.java
+++ src/entities/Product.java
@@ -5,29 +5,35 @@ public class Product {
     public double price;
     public int quantity;
 
-    //criar um contrutor para garantir que o usuário insira os dados
-    public Product (String name, double price, int quantity) {
+    //sobrecarga padrao
+    public Product () {
+    }
+
+    public Product(String name, double price, int quantity) {
         //this - serve para referenciar o objeto(Public String name)
         this.name = name;
         this.price = price;
         this.quantity = quantity;
     }
 
+    //Sobrecarga - cria uma nova versão para o construtor
+    public Product(String name, double price){
+        this.name = name;
+        this.price = price;
+    }
+
     public double totalValueInStock (){
         return price * quantity;
     }
 
-    //usa o valor do quantity(da classe) e soma com o quantity(atributo)
     public void addProducts(int quantity){
         this.quantity += quantity;
     }
 
-    //saida estoque
     public void removeProducts(int quantity) {
         this.quantity -= quantity;
     }
 
-    //toString -> converter o objeto produto em string
     public String toString() {
         return name + ", $" + String.format("%.2f", price) + ", " + quantity + " units, Total: $" + String.format("%.2f", totalValueInStock());
     }
